2deb74dd9b3bb6ef8cedc1d21446657bef46dd4f
Igor G <106555763+Naomius@users.noreply.github.com>
2023-06-29 17:24:37 +0300

    some fixes

--- frontend/src/app/shared/services/user.service.ts
+++ frontend/src/app/shared/services/user.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {map, Observable, tap} from "rxjs";
+import {Observable} from "rxjs";
 import {UserInfoType} from "../../../types/user-info.type";
 import {DefaultResponseType} from "../../../types/default-response.type";
 import {environment} from "../../../environments/environment.development";
-import {AuthService} from "../../core/auth/auth.service";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private http: HttpClient,
-              private authService: AuthService) { }
+  constructor(private http: HttpClient) { }
 
   getUserInfo(): Observable<UserInfoType | DefaultResponseType> {
     return this.http.get<UserInfoType | DefaultResponseType>(environment.api + 'users', {
